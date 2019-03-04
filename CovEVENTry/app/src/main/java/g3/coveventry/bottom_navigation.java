dcd027c097ab639e7deb936e2dcda48412504c55
bakerj18 <bakerj18@coventry.ac.uk>
2019-03-04 14:01:54 +0000

    added pressed icons and about us page

--- CovEVENTry/app/src/main/java/g3/coveventry/bottom_navigation.java
+++ CovEVENTry/app/src/main/java/g3/coveventry/bottom_navigation.java
@@ -34,7 +34,7 @@ public class bottom_navigation extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom_navigation);
+        setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
